032ef8bbedb90753482b53d25507e7ae3c7b2592
Manfred Endres <manfred.endres@tslarusso.de>
2015-09-17 17:21:46 +0200

    add code climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,6 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'lanyard'
 require 'lanyard/security'
 require 'lanyard/lanyard_cmd'
+
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
