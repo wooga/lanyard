aa9fc556a4ee55b175eead02758d313fe0b8918d
Manfred Endres <manfred.endres@tslarusso.de>
2015-09-17 17:26:50 +0200

    move code coverage to top in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'lanyard'
 require 'lanyard/security'
 require 'lanyard/lanyard_cmd'
-
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
